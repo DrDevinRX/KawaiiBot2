44c2ca988f5ac06e96d34cfbd709ce4c1e3e39ca
Devin <drdevinrx@gmail.com>
2020-08-07 00:53:01 -0400

    Added Helper class and updated Commands to use Helper

---
+++ KawaiiBot2/Helper/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Discord;
+
+namespace KawaiiBot2.Helper
+{
+    class Helper
+    {
+        private static readonly Random random = new Random();
+
+        public static T ChooseRandomItem<T>(T[] items)
+            => items[random.Next(items.Count())];
+
+        public static T ChooseRandomItem<T>(IEnumerable<T> items) 
+            => items.ElementAt(random.Next(items.Count()));
+
+        public static string GuildUserNameOrNickName(IGuildUser user)
+            => user.Nickname ?? user.Username ?? "User"; // If user is null, return User
+    }
+}
--- KawaiiBot2/Modules/Commands.cs
+++ KawaiiBot2/Modules/Commands.cs
@@ -1,15 +1,12 @@
 ﻿using Discord;
 using Discord.Commands;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace KawaiiBot2.Modules
 {
     public class Commands : ModuleBase<SocketCommandContext>
     {
-        private static readonly Random Rng = new Random();
         private static readonly string[] SlotIcons = { "🍎", "🍊", "🍐", "🍋", "🍉", "🍇", "🍓", "🍒" };
         private static readonly string[] EightBallResponses = 
         {   "Yes", "No", "Take a wild guess...",
@@ -34,37 +31,35 @@ namespace KawaiiBot2.Modules
         {
             string[] finalSlots = new string[3];
             string winMessage;
-            Dictionary<string, int> occuranceCount = new Dictionary<string, int>();
+            Dictionary<string, int> occurrenceCount = new Dictionary<string, int>();
 
             for (int i = 0; i < 3; i++)
             {
-                string slot = SlotIcons[Rng.Next(SlotIcons.Length)];
+                string slot = Helper.Helper.ChooseRandomItem(SlotIcons);
                 finalSlots[i] = slot;
-                // Add to the dictionary for easy occurance counting
-                if (occuranceCount.ContainsKey(slot))
+                // Add to the dictionary for easy occurrence counting
+                if (occurrenceCount.ContainsKey(slot))
                 {
-                    occuranceCount[slot]++;
+                    occurrenceCount[slot]++;
                 }
                 else
                 {
-                    occuranceCount[slot] = 1;
+                    occurrenceCount[slot] = 1;
                 }
             }
 
             // If we have more than one key, we didn't win
-            if (occuranceCount.Count > 1)
+            if (occurrenceCount.Count > 1)
             {
-                winMessage = (occuranceCount.Count > 2) ? "and lost..." : $"and almost won ({occuranceCount.Count}/{finalSlots.Length})";
+                winMessage = (occurrenceCount.Count > 2) ? "and lost..." : $"and almost won ({occurrenceCount.Count}/{finalSlots.Length})";
             }
             else
             {
                 winMessage = $"and won! \uD83C\uDF89";
             }
 
-            var guildUser = Context.Message.Author as IGuildUser;
-
             return ReplyAsync(
-                            $"**{guildUser.Nickname ?? guildUser.Username}** rolled the slots...\n" +
+                            $"**{Helper.Helper.GuildUserNameOrNickName(Context.Message.Author as IGuildUser)}** rolled the slots...\n" +
                             $"**[ {string.Join(" ", finalSlots)} ]**\n" +
                             $"{winMessage}");
         }
@@ -89,7 +84,7 @@ namespace KawaiiBot2.Modules
             EmbedBuilder embedBuilder = new EmbedBuilder();
 
             var guildUser = user ?? (Context.Message.Author as IGuildUser);
-            return ReplyAsync($"**{guildUser.Nickname ?? guildUser.Username}** joined on {guildUser.JoinedAt}");
+            return ReplyAsync($"**{Helper.Helper.GuildUserNameOrNickName(guildUser)}** joined on {guildUser.JoinedAt}");
         }
 
         [Command("choose")]
@@ -102,7 +97,7 @@ namespace KawaiiBot2.Modules
                 return ReplyAsync("Separate your choices (at least 2) with \"|\"");
             }
 
-            return ReplyAsync($"I choose this: **{choices[Rng.Next(choices.Length)].Trim()}**");
+            return ReplyAsync($"I choose this: **{Helper.Helper.ChooseRandomItem(choices).Trim()}**");
         }
 
         [Command("8ball")]
@@ -115,7 +110,7 @@ namespace KawaiiBot2.Modules
 
             return ReplyAsync(
                             $"🎱 **Question: **{question.Trim()}\n" +
-                            $"**Answer: ** {EightBallResponses[Rng.Next(EightBallResponses.Length)]}");
+                            $"**Answer: ** {Helper.Helper.ChooseRandomItem(EightBallResponses)}");
         }
     }
 }
