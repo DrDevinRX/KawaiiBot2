070c9a2663efc1b450a87d406a65509a373686ab
hitomi <43500169+nanami-hitomi@users.noreply.github.com>
2020-09-01 10:13:07 -0400

    slots with numbers more or less than 3

--- KawaiiBot2/Modules/Slots.cs
+++ KawaiiBot2/Modules/Slots.cs
@@ -15,13 +15,23 @@ namespace KawaiiBot2.Modules
 
         [Command("slots")]
         [Summary("Roll the slot machine. may rngesus guide your path.")]
-        public Task SlotsCmd()
+        public Task SlotsCmd(int n = 3)
         {
+            if (n < 2 || n > 100)
+                return ReplyAsync("Nope. No. Nope. No. No can do.");
 
-            string[] finalSlots = Enumerable.Range(0, 3).Select(i => Helpers.ChooseRandom(SlotIcons)).ToArray();
+
+            string[] finalSlots = Enumerable.Range(0, n).Select(i => Helpers.ChooseRandom(SlotIcons)).ToArray();
             if (rigged && (!riggedUserID.HasValue || riggedUserID == Context.User.Id))
             {
+                if (riggedTo.Length == 1)
+                {
+                    var tmp = riggedTo[0];
+                    riggedTo = new string[n];
+                    for (int i = 0; i < n; i++) riggedTo[i] = tmp;
+                }
                 finalSlots = riggedTo;
+                n = riggedTo.Length;
                 rigged = false;
             }
 
@@ -29,10 +39,10 @@ namespace KawaiiBot2.Modules
 
             string winMessage = "and lost....";
 
-            if (finalSlots[0] == finalSlots[1] && finalSlots[1] == finalSlots[2])
+            if (finalSlots.All(s => s == finalSlots[0]))
                 winMessage = "and won! \uD83C\uDF89";
-            else if (finalSlots[0] == finalSlots[1] || finalSlots[1] == finalSlots[2] || finalSlots[0] == finalSlots[2])
-                winMessage = "and almost won (2/3)";
+            else if (finalSlots.Count(s => s == finalSlots[0]) == n - 1 || finalSlots.Count(s => s == finalSlots[1]) == n - 1)
+                winMessage = $"and almost won ({n - 1}/{n})";
 
 
 
@@ -58,23 +68,19 @@ namespace KawaiiBot2.Modules
             rigTo = rigTo.Replace(" ", "");
             if (rigTo.Length == 2 && SlotIcons.Contains(rigTo))
             {
-                Rig(new string[] { rigTo, rigTo, rigTo }, userID);
+                Rig(new string[] { rigTo }, userID);
                 return ReplyAsync($"*Bumps the slots* Feels {rigTo}.");
-            }
-            else if (rigTo.Length == 6)
-            {
-                var tmpRig = Enumerable.Range(0, 3).Select(i => rigTo.Substring(2 * i, 2)).Where(c => SlotIcons.Contains(c)).ToArray();
-                if (tmpRig.Length == 3)
-                {
-                    Rig(tmpRig, userID);
-                    return ReplyAsync($"*Bumps the slots* Feels {rigTo}.");
-                }
-                else
-                    return ReplyAsync("W-what! I would never!");
             }
             else
             {
-                return ReplyAsync("Syntax fail.");
+                var tmpRig = Enumerable.Range(0, rigTo.Length / 2).Select(i => rigTo.Substring(2 * i, 2)).Where(c => SlotIcons.Contains(c)).ToArray();
+                if (tmpRig.Length < 2)
+                    return ReplyAsync("Huh?");
+                if (tmpRig.Length > 100)
+                    return ReplyAsync("You've gone batty.");
+                Rig(tmpRig, userID);
+                return ReplyAsync($"*Bumps the slots* Feels {rigTo}.");
+
             }
         }
 
