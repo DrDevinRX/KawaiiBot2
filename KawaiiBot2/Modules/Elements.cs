2e0675308d48eafbac79f60a3145ea62113ae87c
hitomi <43500169+nanami-hitomi@users.noreply.github.com>
2020-10-18 13:07:43 -0400

    Update Elements.cs

--- KawaiiBot2/Modules/Elements.cs
+++ KawaiiBot2/Modules/Elements.cs
@@ -11,6 +11,7 @@ using KawaiiBot2.Services;
 using Newtonsoft.Json.Schema;
 using Newtonsoft.Json;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 
 namespace KawaiiBot2.Modules
 {
@@ -81,5 +82,51 @@ namespace KawaiiBot2.Modules
             await (Context.User as IGuildUser).RemoveRoleAsync(role);
             await ReplyAsync("Removed role.");
         }
+
+        [Command("lewdme")]
+        [Summary("Give yourself the 18+ role for access to the porn channel")]
+        [RequireContext(ContextType.Guild)]
+        public async Task giveMeR18Role([Remainder] string ack = null)
+        {
+
+            var server = Context.Guild;
+            //only in grace's impact server
+            if (server.Id != 761750996610318376)
+            {
+
+                return;
+            }
+            var userHashCode = Context.User.Id.GetHashCode();
+            var ackCode = (userHashCode & 0x7FFF_FFFF) >> 50;
+            var ackCodeStr = $"I acknowledge with ack code {ackCode}";
+            if (ack == null)
+            {
+                await ReplyAsync("By accepting this, you are saying that you are above 18 and allowed to view porn stuff.\n" +
+                    "This is the really really heavy stuff, the less bad stuff is in lewd art channel" +
+                    "Type " +
+                    $"`+lewdme {ackCodeStr}` to accept the disclaimer and get the role.\n" +
+                    $"(Make sure to type it exactly or copy-paste! With nothing more than what's in the ``!)");
+                return;
+            }
+            var role = Context.Guild.GetRole(767142223388344330);//lewd role id
+            if (ack == ackCodeStr)
+            {
+                await (Context.User as IGuildUser).AddRoleAsync(role);
+                await ReplyAsync("Lewd role added.");
+            }
+            else if (ack == "remove")
+            {
+                await (Context.User as IGuildUser).RemoveRoleAsync(role);
+                await ReplyAsync("Lewd role removed.");
+            }
+            else
+            {
+                await (Context.User as IGuildUser).RemoveRoleAsync(role);
+                await ReplyAsync("Lewd role denied/removed.\n" +
+                    "If you wanted it, did you get your ack code correct? Use without any parameters to see it.");
+            }
+
+        }
+
     }
 }
