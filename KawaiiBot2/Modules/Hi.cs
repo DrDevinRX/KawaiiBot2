4c47bb4cfd81857f9d75d7656409de1686dca8ff
hitomi <43500169+nanami-hitomi@users.noreply.github.com>
2021-06-21 04:46:57 +0900

    Update Hi.cs

--- KawaiiBot2/Modules/Hi.cs
+++ KawaiiBot2/Modules/Hi.cs
@@ -13,27 +13,49 @@ namespace KawaiiBot2.Modules
     {
 
         [Command("hi")]
+        [Alias("hey")]
         [Summary("Because Father Servo's always down.")]
 
         public Task HiCmd()
         {
-            if (yesHey)
-            {
-                yesHey = false;
-                return ReplyAsync("HEY");
-            }
-            return ReplyAsync(new Random().Next(10) == 7 ? "HEY" : "hi");
+            var win = new Random().Next(10) == 7;
+            var rig = yesHey ^ alwaysHey;
+            yesHey = false;
+            win |= rig;
+            return ReplyAsync(win ? "HEY" : "hi");
         }
 
         private volatile static bool yesHey;
+        private volatile static bool alwaysHey;
         [Command("righi")]
         [Summary("Because some people need to win everything.")]
         [HiddenCmd]
         public Task RigHi()
         {
-            var exeName = Helpers.GetName(Context.User);
             yesHey = true;
             return ReplyAsync("HEY!");
+        }
+
+        [Command("permarighi")]
+        [Alias("alwayshey", "permahey")]
+        [Summary("There is no choice in the matter.")]
+        [DevOnlyCmd]
+        public Task PermaRigHi(bool rig = true)
+        {
+            if (!Helpers.devIDs.Contains(Context.User.Id))
+            {
+                return ReplyAsync("Such a mode does not exist.");
+            }
+            alwaysHey = rig;
+            return ReplyAsync("HEYHEYHEYHEYHEY");
+        }
+
+        [Command("navi")]
+        [Summary("Hey, Listen!")]
+        [HiddenCmd]
+        public Task Navi()
+        {
+            return ReplyAsync("HEY, listen!");
         }
     }
 }
